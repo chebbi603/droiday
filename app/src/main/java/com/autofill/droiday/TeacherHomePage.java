e63c96e5ba841816e6dc5be64234534d4a214a6a
MankaiAdam <mankai.adam@gmail.com>
2022-03-24 03:29:53 +0100

    changed app gradle

--- app/src/main/java/com/autofill/droiday/TeacherHomePage.java
+++ app/src/main/java/com/autofill/droiday/TeacherHomePage.java
@@ -1,14 +1,128 @@
 package com.autofill.droiday;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class TeacherHomePage extends AppCompatActivity {
+
+    String name;
+    TextView UserName;
+    ImageView avatarImage;
+    Button btn2;
+    private FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    FirebaseFirestore db;
+    private Target mTarget;
+    TextView title;
+    Picasso picasso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_home_page);
+        setContentView(R.layout.activity_home_page);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        db = FirebaseFirestore.getInstance();
+        UserName = findViewById(R.id.UserNameTeacher);
+        avatarImage = findViewById(R.id.AvatarImgTeacher);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        btn2 = findViewById(R.id.TeacherBtn2);
+        title = findViewById(R.id.title_homeTeacher);
+
+
+        db.collection("users")
+                .document(mUser.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                name = "" + document.getData().get("first");
+                                UserName.setText(name);
+                                Log.d("URL log", "onComplete: "+ mUser.getPhotoUrl().toString());
+                                loadImage(mUser.getPhotoUrl().toString());
+
+                            } else {
+                                //Toast.makeText(MainActivity.this, "Document Not Found", Toast.LENGTH_SHORT).show();
+                                //Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            //Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                            //Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public Bitmap getCroppedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output;
+    }
+
+    void loadImage(String url) {
+
+        mTarget = new Target() {
+            @Override
+            public void onBitmapLoaded (Bitmap bitmap, Picasso.LoadedFrom from){
+                bitmap = getCroppedBitmap(bitmap);
+                avatarImage.setImageBitmap(bitmap);
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+
+        picasso.get()
+                .load(url)
+                .into(mTarget);
     }
 }
