7bd7f121c4e4e9e36bc8d6e2db4499b43e2f0842
chebbi603 <chebbi603@gmail.com>
2022-03-24 02:58:05 +0100

    v1.0.0.3

--- app/src/main/java/com/autofill/droiday/AccountSetUpActivity.java
+++ app/src/main/java/com/autofill/droiday/AccountSetUpActivity.java
@@ -44,7 +44,7 @@ public class AccountSetUpActivity extends AppCompatActivity implements AdapterVi
     String[] Levels = { "1ere" , "2eme", "3eme", "4eme", "5eme", "6eme", "7eme", "8eme", "9eme"};
     EditText FirstName, LastName;
     Button SetUpBtn , SetUpBtn1, SetUpBtn2;
-    RadioButton Teacher, Student;
+    Button Teacher, Student;
     String firstname, lastname;
     String email = "";
     String password = "";
@@ -71,8 +71,8 @@ public class AccountSetUpActivity extends AppCompatActivity implements AdapterVi
         SetUpBtn = findViewById(R.id.SetUpBtn);
         SetUpBtn1 = findViewById(R.id.SetUpBtn1);
         SetUpBtn2 = findViewById(R.id.SetUpBtn2);
-        Student = findViewById(R.id.Student);
-        Teacher = findViewById(R.id.Teacher);
+        Student = findViewById(R.id.student);
+        Teacher = findViewById(R.id.teacher);
         TextView title = findViewById(R.id.titleinstance);
         TextView desc = findViewById(R.id.desc);
         TextView title2 = findViewById(R.id.titleinstance2);
@@ -94,7 +94,10 @@ public class AccountSetUpActivity extends AppCompatActivity implements AdapterVi
         ArrayAdapter aa = new ArrayAdapter(this,R.layout.spinner,Levels);
         aa.setDropDownViewResource(R.layout.dropdown);
         LvlChoice.setAdapter(aa);
-
+        Student.setBackground(getDrawable(R.drawable.checkboxisoff));
+        Student.setTextColor(Color.BLACK);
+        Teacher.setBackground(getDrawable(R.drawable.checkboxisoff));
+        Teacher.setTextColor(Color.BLACK);
         const1.setVisibility(View.VISIBLE);
         const2.setVisibility(View.INVISIBLE);
         const1.setBackgroundColor(Color.rgb(254,234,148));
@@ -102,6 +105,9 @@ public class AccountSetUpActivity extends AppCompatActivity implements AdapterVi
         desc.setText("Please provide us with your full name");
         title2.setTextColor(Color.rgb(45,124,225));
         final int[] isTrue = {0};
+
+        final boolean[] isStudent = {false};
+        final boolean[] isTeacher = {false};
 
         SetUpBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -122,17 +128,100 @@ public class AccountSetUpActivity extends AppCompatActivity implements AdapterVi
             }
         });
 
+        Student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isStudent[0] = true;
+                isTeacher[0] = false;
+                Student.setBackground(getDrawable(R.drawable.checkboxison));
+                Student.setTextColor(Color.WHITE);
+                Teacher.setBackground(getDrawable(R.drawable.checkboxisoff));
+                Teacher.setTextColor(Color.BLACK);
+
+            }
+        });
+        Teacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isStudent[0] = false;
+                isTeacher[0] = true;
+                Teacher.setBackground(getDrawable(R.drawable.checkboxison));
+                Teacher.setTextColor(Color.WHITE);
+                Student.setBackground(getDrawable(R.drawable.checkboxisoff));
+                Student.setTextColor(Color.BLACK);
+
+            }
+        });
+
         SetUpBtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(Student.isChecked() || Teacher.isChecked()){
-                    if(Teacher.isChecked()) radio = 1;
+                if(isStudent[0]|| isTeacher[0]){
+                    if(isTeacher[0]) radio = 1;
+                    firstname = firstname.substring(0, 1).toUpperCase() + firstname.substring(1);
+                    lastname = lastname.substring(0, 1).toUpperCase() + lastname.substring(1);
+
+                    //Update Username and Profile Picture
+                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                            .setDisplayName(firstname)
+                            //.setPhotoUri()
+                            .build();
+                    mUser.updateProfile(profileUpdates)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        //Toast.makeText(AccountSetUpActivity.this, "Name Updated To"+firstname, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                    // Create a new user with a first and last name
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("first", firstname);
+                    user.put("last", lastname);
+                    user.put("avatar", "0");
+                    user.put("type", "teacher");
+                    user.put("xp", 0);
+                    //
+                    // Add a new document with a generated ID
+                    DocumentReference doc = db.collection("users").document(mUser.getUid());
+                    doc.set(user);
+
+                    //create Participation
+                    Map<String, Object> part = new HashMap<>();
+                    doc.collection("Participation").document("-1").set(part);
+
+                    //Sign Out
+                    if(email!=null && password!=null ) {
+                        FirebaseAuth.getInstance().signOut();
+
+                        //Sign In Again
+                        mAuth.signInWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(AccountSetUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            mUser = mAuth.getCurrentUser();
+                                            //Go to MainActivity
+                                            updateUI(mUser);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(AccountSetUpActivity.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    }else{updateUI(mUser);}
+                }else {
                     const1.startAnimation(fadeout);
                     const1.setVisibility(View.INVISIBLE);
                     const2.startAnimation(fadein);
                     const2.setVisibility(View.VISIBLE);
                     desc2.setText("Select your class from the dropdown below");
                     welcome.setText("Welcome " + firstname + " !");
+
                 }
             }
         });
